2afc55962509dad707cb1cb90270ddff070d4073
Mehedi1324 <you@example.com>
2022-01-14 22:34:54 +0600

    df

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
-const fileUpload = require("express-fileUpload");
+// const fileUpload = require("express-fileUpload");
 
 
 const serviceAccount = require("./docsportal-firebaseKey.json")
@@ -16,7 +16,7 @@ admin.initializeApp({
 });
 
 app.use(cors());
-app.use(fileUpload());
+// app.use(fileUpload());
 app.use(express.json());
 
 
